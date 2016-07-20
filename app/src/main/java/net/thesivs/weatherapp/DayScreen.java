0f79fef261967c3461cb1b586bdc8d58a41695e2
James F <jfsivaeb@gmail.com>
2016-07-19 22:08:21 -0700

    JSON Corrections
    Fixed few JSON getString, need to design GUI.

--- app/src/main/java/net/thesivs/weatherapp/DayScreen.java
+++ app/src/main/java/net/thesivs/weatherapp/DayScreen.java
@@ -11,8 +11,10 @@ import android.view.MenuItem;
 import org.json.*;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -30,7 +32,15 @@ public class DayScreen extends AppCompatActivity {
         String zipCode = "94577";
         String url_preZip = "http://api.openweathermap.org/data/2.5/weather?zip=";
         String url_postZip = "&units=imperial&appid=c15dc5a78ba5919b373dfb1d589d6b2d";
-        new WeatherDataAsyncTask().execute(url_preZip + zipCode + url_postZip);
+
+        URL url = null;
+        try {
+            url = new URL(url_preZip + zipCode + url_postZip);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        new WeatherDataAsyncTask().execute(url);
     }
 
     @Override
@@ -56,54 +66,84 @@ public class DayScreen extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public class WeatherDataAsyncTask extends AsyncTask<String, Void, Boolean> {
+    public class WeatherDataAsyncTask extends AsyncTask<URL, Void, String> {
         @Override
-        protected Boolean doInBackground(String... strings) {
+        protected String doInBackground(URL... params) {
             HttpURLConnection httpURLConnection = null;
+            StringBuilder stringBuilder = new StringBuilder();
 
             try {
-                URL url = new URL(strings[0]);
+                URL url = params[0];
                 httpURLConnection = (HttpURLConnection) url.openConnection();
-                String input = new BufferedInputStream(httpURLConnection.getInputStream()).toString();
+                httpURLConnection.setRequestMethod("GET");
 
                 if(httpURLConnection.getResponseCode() == 200) {
-                    JSONObject jsonObject = new JSONObject(input);
+                    InputStream in = new BufferedInputStream(httpURLConnection.getInputStream());
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in));
 
-                    JSONArray weatherArray = new JSONArray(); //[0].main, description, icon
-                    JSONObject mainObject = new JSONObject(); //temp, pressure, humitidy, temp_min, temp_max
-                    JSONObject sysObject = new JSONObject(); //sunrise, sunset
-                    JSONObject rainObject = new JSONObject(); //rain_3h
-
-                    weatherArray = jsonObject.getJSONArray("weather");
-                    mainObject = jsonObject.getJSONObject("main");
-                    sysObject = jsonObject.getJSONObject("sys");
-                    rainObject = jsonObject.getJSONObject("rain");
-
-                    /* ALL OBJECTS/ARRAYS DEFINED, TIME TO SET myWeather. variables */
-                    myWeather.setDt(jsonObject.get("dt").toString());
-                    myWeather.setCity_name(jsonObject.get("name").toString());
-
-                    String rain = (rainObject.get("rain_3h").toString() == null) ? "0" : jsonObject.getJSONObject("rain").toString();
-                    myWeather.setRain_3h(rain);
-
+                    String line;
+                    while ((line = bufferedReader.readLine()) != null) {
+                        stringBuilder.append(line);
+                    }
                 }
-
             } catch (MalformedURLException e) {
                 e.printStackTrace();
             } catch (IOException e) {
                 e.printStackTrace();
-            } catch (JSONException e) {
-                e.printStackTrace();
             } finally {
-                httpURLConnection.disconnect();
+                if(httpURLConnection != null )
+                    httpURLConnection.disconnect();
             }
 
-            return false;
+            Log.e("values", stringBuilder.toString());
+            return stringBuilder.toString();
         }
 
         @Override
-        protected void onPostExecute(Boolean aBoolean) {
-            super.onPostExecute(aBoolean);
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            try {
+                JSONObject jsonObject = new JSONObject(result);
+
+                JSONArray weatherArray = new JSONArray(); //[0].main, description, icon
+                JSONObject mainObject = new JSONObject(); //temp, pressure, humitidy, temp_min, temp_max
+                JSONObject sysObject = new JSONObject(); //sunrise, sunset
+                JSONObject rainObject = new JSONObject(); //rain_3h
+
+                weatherArray = jsonObject.getJSONArray("weather");
+                mainObject = jsonObject.getJSONObject("main");
+                sysObject = jsonObject.getJSONObject("sys");
+
+                /* ALL OBJECTS/ARRAYS DEFINED, TIME TO SET myWeather. variables */
+                if(jsonObject.isNull("rain")) {
+                    rainObject = null;
+                    myWeather.setRain_3h("0");
+                }
+                else {
+                    rainObject = jsonObject.getJSONObject("rain");
+                    myWeather.setRain_3h(rainObject.getString("rain_3h").toString());
+                }
+
+                myWeather.setDt(jsonObject.getString("dt").toString());
+                myWeather.setCity_name(jsonObject.getString("name").toString());
+
+                myWeather.setWeather_description(weatherArray.getJSONObject(0).getString("description").toString());
+                myWeather.setWeather_main(weatherArray.getJSONObject(0).getString("main").toString());
+                myWeather.setWeather_icon(weatherArray.getJSONObject(0).getString("icon").toString());
+
+                myWeather.setMain_temp(mainObject.getString("temp").toString());
+                myWeather.setMain_temp_high(mainObject.getString("temp_max").toString());
+                myWeather.setMain_temp_min(mainObject.getString("temp_min").toString());
+                myWeather.setMain_humidity(mainObject.getString("humidity").toString());
+
+                myWeather.setSys_sunrise(sysObject.getString("sunrise").toString());
+                myWeather.setSys_sunset(sysObject.getString("sunset").toString());
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
+
